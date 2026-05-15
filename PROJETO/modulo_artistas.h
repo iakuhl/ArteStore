942e9e5925a2558aa7b7d82bdeba9bf49e42d1fa
iakuhl <ianodark@gmail.com>
2026-05-15 15:39:47 -0300

    Add documentation header to modulo_artistas.h
    Add header comments to modulo_artistas.h

---
+++ PROJETO/modulo_artistas.h
@@ -0,0 +1,19 @@
+/*
+ * Projeto: Sistema de Curadoria de Obras de Artes
+ * Arquivo: modulo_artistas.h
+ * Autor: Iano de Oliva Kuhlmann
+ * Colaboradores: chat.deepseek.com
+ * Link de colaboração: https://chat.deepseek.com/share/jil3nf8yyu9wwz0h8l
+ * Disciplina: APR2
+ * Professora: Dra. Eloize Rossi Marques Seno
+ */
+
+
+#ifndef MODULO_ARTISTAS_H
+#define MODULO_ARTISTAS_H
+
+#include "estruturas.h"
+
+void moduloArtistas(ListaArtistas *lista);
+
+#endif
