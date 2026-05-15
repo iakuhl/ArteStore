b9e3bde2b5bae7fc160b0b61a46e23feec630948
iakuhl <ianodark@gmail.com>
2026-05-15 14:23:38 -0300

    Add project header comments to listas.c
    Added project header comments with author and collaboration details.

--- PROJETO/listas.c
+++ PROJETO/listas.c
@@ -1,4 +1,12 @@
-// Feito integralmente pelo DeepSeek, deverá ser revisado
+/*
+ * Projeto: Sistema de Curadoria de Obras de Artes
+ * Arquivo: listas.c
+ * Autor: Iano de Oliva Kuhlmann
+ * Colaboradores: chat.deepseek.com
+ * Link de colaboração: https://chat.deepseek.com/share/jil3nf8yyu9wwz0h8l
+ * Disciplina: APR2
+ * Professora: Dra. Eloize Rossi Marques Seno
+ */
 
 
 #include <stdlib.h>
