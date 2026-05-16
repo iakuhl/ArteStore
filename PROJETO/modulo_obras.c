7f1f61a1ae25bee6b798f40e771b3fb48d80c933
iakuhl <ianodark@gmail.com>
2026-05-15 21:10:24 -0300

    Add menuObras function for obra management

---
+++ PROJETO/modulo_obras.c
@@ -0,0 +1,13 @@
+
+
+
+int menuObras()
+{
+    printf("### MENU OBRAS ###\n");
+    printf("1 - Cadastrar Obra\n");
+    printf("2 - Listar Obras\n");
+    printf("3 - Buscar Obra por ID\n");
+    printf("4 - Voltar ao Menu Principal\n");
+
+    return escolherOpcao(1, 4);
+}
