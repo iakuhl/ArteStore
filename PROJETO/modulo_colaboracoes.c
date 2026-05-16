c77cd0ebc665e1b3a91a34975f9e75cf686347c7
iakuhl <ianodark@gmail.com>
2026-05-15 21:10:48 -0300

    Add menu for collaborations in modulo_colaboracoes.c

---
+++ PROJETO/modulo_colaboracoes.c
@@ -0,0 +1,9 @@
+int menuColaboracoes()
+{
+    printf("### MENU COLABORACOES ###\n");
+    printf("1 - Cadastrar Colaboracao\n");
+    printf("2 - Listar Colaboracoes\n");
+    printf("3 - Voltar ao Menu Principal\n");
+
+    return escolherOpcao(1, 3);
+}
