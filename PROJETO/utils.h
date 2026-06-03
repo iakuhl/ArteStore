d7c4066a7f6a0870471e94f4a8032b1877f4f152
iakuhl <ianodark@gmail.com>
2026-06-03 10:16:38 -0300

    Add imprimeValor function to utils.h
    Added a function to print values formatted in Brazilian reais.

--- PROJETO/utils.h
+++ PROJETO/utils.h
@@ -28,5 +28,6 @@ bool removeMascaraCPF(char *cpf); // Remove pontos e hífen de um CPF, mantendo 
 bool lerSimNao(char *resposta); // Função para ler uma resposta de sim ou não do usuário, retornando true para sim e false para não.
 int escolherOpcao(int min, int max); // Função para ler a escolha do usuário em um menu, garantindo que seja um número inteiro dentro do intervalo permitido.
 void imprimeCPF(const char *cpf); // Função para imprimir um CPF formatado (XXX.XXX.XXX-XX).
+void imprimeValor(int valor); // Função para imprimir valor em reais formatado (R$ 00.000,00).
 
 #endif // UTILS_H
